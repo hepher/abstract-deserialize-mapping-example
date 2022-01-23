c6ebc0bae334b51832dcf29cdc65476572d754f8
Cristopher Tinnirello <cristopher.tinnirello@dedalus.eu>
2022-01-23 18:51:26 +0100

    clean up unused dependency

--- src/main/java/org/deserialize/test/TestDeserialize.java
+++ src/main/java/org/deserialize/test/TestDeserialize.java
@@ -4,11 +4,12 @@ import com.deserialize.mapping.main.AbstractDeserializeMapping;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @Component
 public class TestDeserialize extends AbstractDeserializeMapping<Test> {
 
-    public TestDeserialize() throws IOException {
+    public TestDeserialize() throws IOException, URISyntaxException {
         super(Test.class, null, null);
     }
 }
--- src/test/java/org/deserialize/abstractdeserialize/AbstractDeserializeApplicationTests.java
+++ src/test/java/org/deserialize/abstractdeserialize/AbstractDeserializeApplicationTests.java
@@ -1,15 +1,15 @@
 package org.deserialize.abstractdeserialize;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class AbstractDeserializeApplicationTests {
 
-	@Test
+//	@Test
 	public void contextLoads() {
 	}
 
