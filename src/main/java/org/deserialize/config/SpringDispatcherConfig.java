4f637aa5c00c215865183a25f2bee8c74dc84427
hepher <tinni.cris@gmail.com>
2023-01-10 17:17:19 +0100

    Update SpringDispatcherConfig.java

--- src/main/java/org/deserialize/config/SpringDispatcherConfig.java
+++ src/main/java/org/deserialize/config/SpringDispatcherConfig.java
@@ -1,4 +1,21 @@
 package org.deserialize.config;
 
-public class SpringDispatcherConfig {
+@Configuration
+public class SpringDispatcherConfig implements WebMvcConfigurer {
+  
+  @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index.html");
+    }
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+
+//        bean.setViewClass(JstlView.class);
+//        bean.setPrefix("/WEB-INF/view/");
+//        bean.setSuffix(".jsp");
+
+        return bean;
+    }
 }
