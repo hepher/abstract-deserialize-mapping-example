ff563fc7527347f3adfa1588ff2858c362780bda
hepher <tinni.cris@gmail.com>
2023-01-10 17:18:06 +0100

    Update WebSocketConfig.java

--- src/main/java/org/deserialize/config/WebSocketConfig.java
+++ src/main/java/org/deserialize/config/WebSocketConfig.java
@@ -1,4 +1,21 @@
 package org.deserialize.config;
 
-public class WebSocketConfig {
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  
+  @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // path prefix where client can subscribe notifications
+        config.enableSimpleBroker("/topic");
+        // topic/<nome_utente>/<tipo_notification>
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // path where the socket is opened ( point to register new SockJS() )
+        registry.addEndpoint("/notificationbroker");
+        registry.addEndpoint("/notificationbroker").withSockJS();
+    }
 }
