bb208ed4f45b918ff2a12fe3e07422bcab94fb0a
hepher <tinni.cris@gmail.com>
2023-06-06 10:45:09 +0200

    Update CustomValue.java

--- src/main/java/org/deserialize/config/resolver/CustomValue.java
+++ src/main/java/org/deserialize/config/resolver/CustomValue.java
@@ -1,4 +1,18 @@
 package org.deserialize.config.resolver;
 
+import springfox.documentation.service.ParameterType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
 public @interface CustomValue {
+    String value() default "X-CustomValue";
+    String name() default "X-CustomValue";
+    String description() default "X-CustomValue";
+    boolean required() default false;
+    ParameterType type() default ParameterType.HEADER;
 }
