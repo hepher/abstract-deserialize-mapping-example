27b07369d98e863a4e34449129dcc938fa93785e
hepher <tinni.cris@gmail.com>
2023-06-06 10:44:00 +0200

    Update CucumberSpringConfiguration.java

--- src/test/java/org/deserialize/cucumber/CucumberSpringConfiguration.java
+++ src/test/java/org/deserialize/cucumber/CucumberSpringConfiguration.java
@@ -1,4 +1,14 @@
 package org.deserialize.cucumber;
 
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.AutoConfigureDataMongo;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@ContextConfiguration
+@CucumberContextConfiguration
+//@AutoConfigureDataMongo
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberSpringConfiguration {
 }
