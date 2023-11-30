40aa0a75f6ed75d71967089d6db95962a092cda1
hepher <tinni.cris@gmail.com>
2023-11-30 18:22:47 +0100

    Delete src/test/java/org/deserialize/cucumber/CucumberRunnerTest.java

--- src/test/java/org/deserialize/cucumber/CucumberRunnerTest.java
+++
@@ -1,9 +0,0 @@
-package org.deserialize.cucumber;
-
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.Suite;
-
-@Suite
-@IncludeEngines("cucumber")
-public class CucumberRunnerTest {
-}
