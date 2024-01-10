68d5ff90b90ae9d7e5dfdb1be1b62e8c20acf510
hepher <tinni.cris@gmail.com>
2024-01-10 14:47:21 +0100

    Update AbstractLoggingAspect.java

--- src/main/java/org/deserialize/config/aspect/spv3/AbstractLoggingAspect.java
+++ src/main/java/org/deserialize/config/aspect/spv3/AbstractLoggingAspect.java
@@ -4,22 +4,11 @@ import com.enelx.bfw.framework.exception.BfwException;
 import com.enelx.bfw.framework.util.LabelUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.exception.ExceptionUtils;
-import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.MDC;
 import org.springframework.util.StopWatch;
-
-import java.util.function.BiConsumer;
 
 @Slf4j
 public abstract class AbstractLoggingAspect {
-
-    protected JoinPointDetail detail;
-    protected String klassType = "AbstractLoggingAspect";
-    protected String transactionId;
-    protected BiConsumer<JoinPointDetail, Object> successConsumer;
-    protected BiConsumer<JoinPointDetail, BfwException> errorConsumer;
-    protected LoggingKlassStrategy loggingKlassStrategy = LoggingKlassStrategy.KLASS_METHOD;
 
     public enum LoggingKlassStrategy {
         EXECUTION_METHOD {
@@ -41,46 +30,46 @@ public abstract class AbstractLoggingAspect {
     @Pointcut("execution(* com.enelx..*.* (..))")
     protected void trackingPackagePointcut() {}
 
-    protected Object proceed(ProceedingJoinPoint joinPoint) throws Throwable {
+    protected Object proceed(LoggingAspectParameter parameter) throws Throwable {
+
+        JoinPointDetail detail = parameter.getDetail();
 
         if (detail == null) {
-            detail = new JoinPointDetail(joinPoint, transactionId);
+            detail = new JoinPointDetail(parameter.getJoinPoint(), parameter.getTransactionId());
         }
 
         StopWatch watcher = new StopWatch();
         try {
-            log.info(LabelUtils.START_LOG_PARAMS + detail.getParameterListAsString(), klassType, loggingKlassStrategy.getKlass(detail), detail.getMethod());
+            log.info(LabelUtils.START_LOG_PARAMS + detail.getParameterListAsString(), parameter.getKlassType(), parameter.getLoggingKlassStrategy().getKlass(detail), detail.getMethod());
             watcher.start("Starting method execution: " + detail.getMethod());
 
-            MDC.put(LabelUtils.TRANSACTION_ID, detail.getTransactionId());
-
-            Object result = joinPoint.proceed();
-            if (successConsumer != null) {
-                successConsumer.accept(detail, result);
+            Object result = parameter.getJoinPoint().proceed();
+            if (parameter.getSuccessConsumer() != null) {
+                parameter.getSuccessConsumer().accept(detail, result);
             }
             return result;
         } catch (BfwException e) {
-            log.error(LabelUtils.EXIT_LOG_HEADER_ERROR, klassType, loggingKlassStrategy.getKlass(detail), detail.getMethod(), e.getLocalizedMessage());
+            log.error(LabelUtils.EXIT_ERROR_LOG_HEADER_ERROR,  parameter.getKlassType(), parameter.getLoggingKlassStrategy().getKlass(detail), detail.getMethod(), e.getLocalizedMessage());
 
-            if (errorConsumer != null) {
-                errorConsumer.accept(detail, e);
+            if (parameter.getErrorConsumer() != null) {
+                parameter.getErrorConsumer().accept(detail, e);
             }
             throw e;
         } catch (Throwable e) {
-            log.error(LabelUtils.EXIT_LOG_HEADER_ERROR, klassType, loggingKlassStrategy.getKlass(detail), detail.getMethod(), e.getLocalizedMessage());
+            log.error(LabelUtils.EXIT_ERROR_LOG_HEADER_ERROR, parameter.getKlassType(), parameter.getLoggingKlassStrategy().getKlass(detail), detail.getMethod(), e.getLocalizedMessage());
             log.error(ExceptionUtils.getStackTrace(e));
 
-            BfwException bfwException = new BfwException(e.getLocalizedMessage(), detail.getTransactionId(), null);
-            if (errorConsumer != null) {
-                errorConsumer.accept(detail, bfwException);
+            BfwException bfwException = new BfwException(e.getLocalizedMessage(), detail.getTransactionId());
+            if (parameter.getErrorConsumer() != null) {
+                parameter.getErrorConsumer().accept(detail, bfwException);
             }
             throw e;
         } finally {
             watcher.stop();
             for (StopWatch.TaskInfo taskInfo : watcher.getTaskInfo()) {
-                log.debug(LabelUtils.TASK_INFO_LOG, klassType, loggingKlassStrategy.getKlass(detail), detail.getMethod(), taskInfo.getTaskName(), taskInfo.getTimeMillis());
+                log.debug(LabelUtils.TASK_INFO_LOG, parameter.getKlassType(), parameter.getLoggingKlassStrategy().getKlass(detail), detail.getMethod(), taskInfo.getTaskName(), taskInfo.getTimeMillis());
             }
-            log.info(LabelUtils.EXIT_LOG_HEADER_RESULT, klassType, loggingKlassStrategy.getKlass(detail), detail.getMethod(), watcher);
+            log.info(LabelUtils.EXIT_LOG_HEADER_RESULT, parameter.getKlassType(), parameter.getLoggingKlassStrategy().getKlass(detail), detail.getMethod(), watcher);
         }
     }
 }
