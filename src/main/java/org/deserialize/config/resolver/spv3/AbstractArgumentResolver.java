64023db05bfe45134c7c45575bbf25f847fa7de2
hepher <tinni.cris@gmail.com>
2024-02-15 09:32:06 +0100

    Create AbstractArgumentResolver.java

---
+++ src/main/java/org/deserialize/config/resolver/spv3/AbstractArgumentResolver.java
@@ -0,0 +1,42 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+public abstract class AbstractArgumentResolver implements HandlerMethodArgumentResolver {
+
+    protected final static ObjectMapper mapper = new ObjectMapper();
+
+    protected String getValueFromRequest(HttpServletRequest request, ParameterType parameterType, String parameterName) throws IOException {
+        return switch (parameterType) {
+            case HEADER -> {
+                if (StringUtils.isNotBlank(request.getHeader(parameterName))) {
+                    yield request.getHeader(parameterName);
+                }
+                yield null;
+            }
+            case BODY -> {
+                if (request.getInputStream() != null) {
+                    JsonNode body = mapper.readTree(StreamUtils.copyToString(request.getInputStream(), Charset.defaultCharset()));
+                    if (body != null) {
+                        JsonNode parameterNode = body.findValue(parameterName);
+                        if (parameterNode != null && !parameterNode.isNull()) {
+                            yield parameterNode.asText();
+                        }
+                    }
+                }
+                yield null;
+            }
+            case QUERY -> {
+                if (StringUtils.isNotBlank(request.getParameter(parameterName))) {
+                    yield request.getParameter(parameterName);
+                }
+                yield null;
+            }
+        };
+    }
