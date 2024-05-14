e19c9c9aec0226b2fe1bd56c35b768ae36f387de
hepher <tinni.cris@gmail.com>
2024-05-14 12:21:26 +0200

    Update ParameterType.java

--- src/main/java/org/deserialize/config/resolver/spv3/ParameterType.java
+++ src/main/java/org/deserialize/config/resolver/spv3/ParameterType.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ParameterType {
-    HEADER("header"), BODY("body"), QUERY("query");
+    HEADER("header"), BODY("body"), QUERY("query"), PATH("path");
 
     final String value;
 }
