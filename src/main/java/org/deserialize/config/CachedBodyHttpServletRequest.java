1715dbb8ec14f14749da3444bd2747e81d96d47c
hepher <tinni.cris@gmail.com>
2024-11-13 15:51:19 +0100

    Update CachedBodyHttpServletRequest.java

--- src/main/java/org/deserialize/config/CachedBodyHttpServletRequest.java
+++ src/main/java/org/deserialize/config/CachedBodyHttpServletRequest.java
@@ -7,12 +7,15 @@ import java.io.*;
 
 public class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
 
-    private final byte[] cachedBody;
+   private final byte[] cachedBody;
+    // holds custom header and value mapping
+    private final Map<String, String> customHeaders;
 
     public CachedBodyHttpServletRequest(HttpServletRequest request) throws IOException {
         super(request);
         InputStream requestInputStream = request.getInputStream();
         this.cachedBody = StreamUtils.copyToByteArray(requestInputStream);
+        this.customHeaders = new HashMap<>();
     }
 
     @Override
@@ -26,3 +29,35 @@ public class CachedBodyHttpServletRequest extends HttpServletRequestWrapper {
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
         return new BufferedReader(new InputStreamReader(byteArrayInputStream));
     }
+
+    public void putHeader(String name, String value){
+        this.customHeaders.put(name, value);
+    }
+
+    public String getHeader(String name) {
+        // check the custom headers first
+        String headerValue = customHeaders.get(name);
+
+        if (headerValue != null) {
+            return headerValue;
+        }
+        // else return from into the original wrapped object
+        return ((HttpServletRequest) getRequest()).getHeader(name);
+    }
+
+    public Enumeration<String> getHeaderNames() {
+        // create a set of the custom header names
+        Set<String> set = new HashSet<>(customHeaders.keySet());
+
+        // now add the headers from the wrapped request object
+        Enumeration<String> e = ((HttpServletRequest) getRequest()).getHeaderNames();
+        while (e.hasMoreElements()) {
+            // add the names of the request headers into the list
+            String n = e.nextElement();
+            set.add(n);
+        }
+
+        // create an enumeration from the set and return
+        return Collections.enumeration(set);
+    }
+}
