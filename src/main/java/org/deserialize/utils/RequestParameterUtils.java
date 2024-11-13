8d67b7be0f685ba74768cd2cfd49144c1292bd6e
hepher <tinni.cris@gmail.com>
2024-11-13 15:47:29 +0100

    Update RequestParameterUtils.java

--- src/main/java/org/deserialize/utils/RequestParameterUtils.java
+++ src/main/java/org/deserialize/utils/RequestParameterUtils.java
@@ -1,6 +1,10 @@
-package com.enelx.bfw.framework.util;
+package com.enel.eic.commons.util;
 
-import com.enelx.bfw.framework.resolver.ParameterType;
+
+import com.enel.eic.commons.constant.HeaderConstants;
+import com.enel.eic.commons.resolver.ParameterType;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,7 +14,10 @@ import org.springframework.web.servlet.HandlerMapping;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RequestParameterUtils {
 
@@ -26,14 +33,28 @@ public class RequestParameterUtils {
             }
             case BODY -> {
                 if (request.getInputStream() != null) {
-                    JsonNode body = mapper.readTree(StreamUtils.copyToString(request.getInputStream(), Charset.defaultCharset()));
-                    if (body != null) {
-                        JsonNode parameterNode = body.findValue(parameterName);
-                        if (parameterNode != null && !parameterNode.isNull()) {
-                            yield parameterNode.asText();
+                    String bodyAsString = StreamUtils.copyToString(request.getInputStream(), Charset.defaultCharset());
+                    try {
+                        // parse body as json object
+                        JsonNode body = mapper.readTree(bodyAsString);
+                        if (body != null) {
+                            JsonNode parameterNode = body.findValue(parameterName);
+                            if (parameterNode != null && !parameterNode.isNull()) {
+                                yield parameterNode.asText();
+                            }
                         }
+                    } catch (JsonProcessingException e) {
+                        // parse body as url encoding
+                        Matcher matcher = Pattern.compile("(\\w+)=(.*?)(?=,\\w+=|$)").matcher(bodyAsString);
+                        while (matcher.find()) {
+                            if (parameterName.equals(matcher.group(1))) {
+                                yield matcher.group(2);
+                            }
+                        }
+
                     }
                 }
+
                 yield null;
             }
             case QUERY -> {
@@ -50,5 +71,16 @@ public class RequestParameterUtils {
                 yield null;
             }
         };
+    }
+
+    public static String getUserIp(HttpServletRequest request) {
+        String xForwardedFor = request.getHeader(HeaderConstants.X_FORWARDED_FOR);
+
+        // Gestisci il caso in cui l'applicazione è dietro un proxy
+        if (StringUtils.isNotBlank(xForwardedFor)) {
+            return xForwardedFor.split(",")[0];
+        }
+
+        return request.getRemoteAddr();
     }
 }
